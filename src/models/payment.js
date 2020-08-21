6a18cb3d88627dc4db5e9f0131d21c0a1588a272
unknown <ajibadeabd@gmail.com>
2020-08-21 23:54:49 +0100

    updating chat funcionalities

--- src/models/payment.js
+++ src/models/payment.js
@@ -3,10 +3,21 @@ const Schema = mongoose.Schema;
 
 const PaymentSchema = new Schema({
   
-  body: {
+  paymentId: {
     type: String,
     required: true 
   },
- });
+  userId: {
+    type: String,
+    required: true 
+  },
+  success: {
+    type: Boolean,
+    default:false 
+  },
+  },
+ {
+  timestamps: true,
+});
 
 module.exports = mongoose.model("Payment", PaymentSchema);
